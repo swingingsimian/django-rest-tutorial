abc666e253f54557cee5bc425ba4df5cbf172176
Nathan Johnson <nath.p.johnson@gmail.com>
2020-11-04 14:26:29 +0000

    2 - Requests & Responses

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from snippets import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
+# These still require trailing slashes or suffixes, e.g. .json
+# or .api (for browsable api docs), this returns the html api view, but .html does not work,
+# despite working if set as header?
 
 urlpatterns = [
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
